ea63ed227945c0b3013dbb890d636fa8b4822e59
Timon Riemslagh <timonriemslagh@gmail.com>
2016-06-05 13:50:25 +0200

    cms voor lijst toe te voegen update

--- public/app/components/cmsNewList/cmsnewlistController.js
+++ public/app/components/cmsNewList/cmsnewlistController.js
@@ -1,8 +1,20 @@
-mainApp.controller('CmsNewListController', function($scope) {
+mainApp.controller('CmsNewListController', function($scope, $compile) {
 
     $('ul.nav li').removeClass('active');
     $('.cms').addClass("active");
 
     $scope.title = "dit is de cms new list pagina";
 
+    $scope.saveList = function() {
+
+        var file = document.querySelector('input[type=file]').files[0];
+
+        console.log(file);
+
+    }
+
+    $scope.addNewItem = function() {
+        var el = $compile('<li class="list-group-item"><new-list-item></new-list-item></li>')($scope);
+        $('.listItems').append( el );
+    }
 });
--- public/app/shared/newListItemDirective/directive.js
+++ public/app/shared/newListItemDirective/directive.js
@@ -1,6 +1,28 @@
 mainApp.directive('newListItem', function() {
+
+    function link(scope, element, attrs) {
+
+        scope.previewPhoto = function(currElement) {
+
+            console.log($(currElement).parent().find('input[type=file]'));
+
+            var file = $(currElement).parent().find('input[type=file]')[0].files[0];
+
+            var reader = new FileReader();
+
+            reader.addEventListener("load", function () {
+                $(currElement).parent().find('img').attr("src", reader.result).show();
+            }, false);
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        }
+    }
+
     return {
         restrict: 'E',
-        templateUrl: '/app/shared/newListItemDirective/template.html'
+        templateUrl: '/app/shared/newListItemDirective/template.html',
+        link: link
     };
 });
