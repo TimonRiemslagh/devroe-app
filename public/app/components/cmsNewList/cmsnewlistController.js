dc2e5e55125ab58f21e52447b07c091cced69b2c
Timon Riemslagh <timonriemslagh@gmail.com>
2016-06-27 18:34:54 +0200

    probleem ipad proberen debuggen

--- public/app/components/cmsNewList/cmsnewlistController.js
+++ public/app/components/cmsNewList/cmsnewlistController.js
@@ -179,8 +179,9 @@ mainApp.controller('CmsNewListController', ['$scope', 'ActiveList', '$filter', '
 
     function uploadFile(file, signedRequest, url){
         const xhr = new XMLHttpRequest();
-        /*
+
         xhr.open('PUT', signedRequest);
+        /*
         xhr.onreadystatechange = () => {
             if(xhr.readyState === 4){
                 $scope.$apply(function() {
@@ -196,7 +197,7 @@ mainApp.controller('CmsNewListController', ['$scope', 'ActiveList', '$filter', '
         xhr.send(file);*/
     }
 
-    alert('test 9');/*
+    alert('test 10');/*
 
     function getSignedRequest(file){
         const xhr = new XMLHttpRequest();
